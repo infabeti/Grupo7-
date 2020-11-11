7522001deab7a3aa5be6bdf38cc14b77200b0a30
JavierGomez <javier.gomezma@elorrieta-errekamari.com>
2020-11-11 16:37:42 +0100

    Creada la clase de gestion de dias

---
+++ GestionCartelera/src/main/java/GestionCartelera/Dia.java
@@ -0,0 +1,49 @@
+package GestionCartelera;
+
+import java.util.ArrayList;
+
+public class Dia {
+	private int MinutosProyeccion;
+	private ArrayList<Pelicula> PelisHoy;
+	
+	Dia(int iMaxMins)
+	{
+		PelisHoy = new ArrayList<Pelicula>();
+		MinutosProyeccion = iMaxMins;
+	}
+	
+	public void AnadirPelicula(Pelicula inPeli)
+	{
+		PelisHoy.add(inPeli);
+	}
+	
+	public int MinutosRestantes()
+	{
+		int minutosOcupados=0;
+		for(int i=0;i<PelisHoy.size();i++)
+		{
+			minutosOcupados+= PelisHoy.get(i).getMinutos();
+		}
+		return MinutosProyeccion-minutosOcupados;
+	}
+	
+	public boolean CabePeli(int iMinutosDuracion)
+	{
+		if(iMinutosDuracion>this.MinutosRestantes())
+			return false;
+		else
+			return true;
+	}
+	
+	public ArrayList<Integer> GetGenerosHoy()
+	{
+		ArrayList<Integer> ListaGeneros = new ArrayList<Integer>();
+		for(int i=0;i<PelisHoy.size();i++)
+		{
+			ListaGeneros.add(PelisHoy.get(i).getGenero());
+		}
+		
+		return ListaGeneros;
+	}
+	
+}
