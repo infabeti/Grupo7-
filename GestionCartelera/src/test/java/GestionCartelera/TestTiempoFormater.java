ac8a98c91323578ec2e10f96e23589639f389bcd
Javier Gomez <javier.gomeszma>
2020-11-15 18:37:53 +0100

    Arreglado el Test del tiempo formater

--- GestionCartelera/src/test/java/GestionCartelera/TestTiempoFormater.java
+++ GestionCartelera/src/test/java/GestionCartelera/TestTiempoFormater.java
@@ -6,9 +6,26 @@ import org.junit.Test;
 
 public class TestTiempoFormater {
 
+	private TiempoFormater T = new TiempoFormater();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TestSoloMinutos() {
+		assertEquals("0:30", T.MinutosAString(30));
+	}
+	
+	@Test
+	public void TestSoloHora() {
+		assertEquals("1:00", T.MinutosAString(60));
+	}
+	
+	@Test
+	public void TestMinutosMenorDiez() {
+		assertEquals("1:01", T.MinutosAString(61));
+	}
+	
+	@Test
+	public void TestHora() {
+		assertEquals("3:33", T.MinutosAString(213));
 	}
 
 }
